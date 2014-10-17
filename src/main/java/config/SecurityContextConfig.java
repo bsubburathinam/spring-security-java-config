713ba9ed68dfebe83781521019c8cf39dd376d45
bsubburathinam <babu.subburathinam@accessdevelopment.com>
2014-10-17 17:41:19 -0600

    Making implicit behaviour explicit.

--- src/main/java/config/SecurityContextConfig.java
+++ src/main/java/config/SecurityContextConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,7 +15,22 @@ public class SecurityContextConfig extends WebSecurityConfigurerAdapter
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+            .inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated() // Ensures that any request to our application requires the user to be authenticated
+                    .and()
+                .formLogin() // Allows users to authenticate with form based login
+                    .and()
+                .httpBasic() // Allows users to authenticate with HTTP Basic authentication
+        // in other words, if user submits credentials using basic auth, fulfill the request
+        // otherwise show basic authentication dialog
+        // If user goes to  http://localhost:8080/app/login, show form based login screen
+        ;
     }
 }
